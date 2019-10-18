942a245af6cea7de2c056ad1fe6178b61a812908
Dmitriy Rabotyagov <drabotyagov@vexxhost.com>
2019-10-18 17:32:17 +0300

    PDF Documentation Build tox target
    This patch adds a `pdf-docs` tox target that will build
    PDF versions of our docs. As per the Train community goal:
    
      https://governance.openstack.org/tc/goals/selected/train/pdf-doc-generation.html
    
    Add sphinxcontrib-svg2pdfconverter to doc/requirements.txt
    to convert our SVGs.
    
    Change-Id: I8783737c2fdfd625e9f015a3630d664e2e2e0cc6
    Story: 2006105

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -38,6 +38,7 @@
 extensions = [
     'openstackdocstheme',
     'sphinx.ext.autodoc',
+    'sphinxcontrib.rsvgconverter',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -62,11 +63,11 @@ description = 'OpenStack-Ansible deploys OpenStack environments using Ansible.'
 project = 'OpenStack-Ansible'
 role_name = 'os_octavia'
 target_name = 'openstack-ansible-' + role_name
-title = 'OpenStack-Ansible Documentation: ' + role_name + 'role'
+title = 'OpenStack-Ansible Documentation: ' + role_name + ' role'
 
 # The link to the browsable source code (for the left hand menu)
 oslosphinx_cgit_link = (
-    "https://git.openstack.org/cgit/openstack/{}".format(target_name)
+    "https://opendev.org/openstack/{}".format(target_name)
 )
 
 # openstackdocstheme options
@@ -219,9 +220,11 @@ htmlhelp_basename = target_name + '-docs'
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, target_name + '.tex',
-     title, author, 'manual'),
+    (master_doc, 'doc-' + target_name + '.tex',
+     title.replace("_", "\_"), author, 'manual'),
 ]
+
+latex_use_xindy = False
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
@@ -279,3 +282,11 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 # texinfo_no_detailmenu = False
+# -- Options for PDF output --------------------------------------------------
+
+pdf_documents = [
+    (master_doc, target_name,
+     title, author)
+]
+
+locale_dirs = ['locale/']
